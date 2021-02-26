05f2da5a512bcfb96dd8b6492366cf0bd59ea383
Olaniyi Olatunji <olaniyiolatunji@gmail.com>
2021-02-26 20:33:42 +0100

    Update Encryption.cs

--- AgencyBanking/Helpers/Encryption.cs
+++ AgencyBanking/Helpers/Encryption.cs
@@ -13,6 +13,16 @@ namespace AgencyBanking.Helpers
 {
     public class Encryption
     {
+        private static byte[] sharedkey =  {
+    0x01, 0x02, 0x03, 0x05, 0x07, 0x0B, 0x0D, 0x11,
+    0x12, 0x11, 0x0D, 0x0B, 0x07, 0x02, 0x04, 0x08,
+    0x01, 0x02, 0x03, 0x05, 0x07, 0x0B, 0x0D, 0x11
+        };
+        private static byte[] sharedvector = {
+    0x01, 0x02, 0x03, 0x05, 0x07, 0x0B, 0x0D, 0x11
+        };
+
+
         private static string EncryptApiRequest(string val)
         {
             using (var ms = new MemoryStream())
@@ -27,8 +37,8 @@ namespace AgencyBanking.Helpers
                    // string sharedvectorval = Settings.AppSettings.RequestSharedVector;
                     sharedvectorval = BinaryToString(sharedvectorval);
 
-                    byte[] sharedkey = System.Text.Encoding.GetEncoding("utf-8").GetBytes(sharedkeyval);
-                    byte[] sharedvector = System.Text.Encoding.GetEncoding("utf-8").GetBytes(sharedvectorval);
+                    byte[] sharedkey = Encoding.GetEncoding("utf-8").GetBytes(sharedkeyval);
+                    byte[] sharedvector = Encoding.GetEncoding("utf-8").GetBytes(sharedvectorval);
 
                     var tdes = new TripleDESCryptoServiceProvider();
                     byte[] toEncrypt = Encoding.UTF8.GetBytes(val);
@@ -176,5 +186,29 @@ namespace AgencyBanking.Helpers
 
             //return (clientId, hash);
         }
+
+        public static String Decrypt(String val)
+        {
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            byte[] toDecrypt = Convert.FromBase64String(val);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, tdes.CreateDecryptor(sharedkey, sharedvector), CryptoStreamMode.Write);
+
+            cs.Write(toDecrypt, 0, toDecrypt.Length);
+            cs.FlushFinalBlock();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        public static String Encrypt(String val)
+        {
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            byte[] toEncrypt = Encoding.UTF8.GetBytes(val);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, tdes.CreateEncryptor(sharedkey, sharedvector), CryptoStreamMode.Write);
+            cs.Write(toEncrypt, 0, toEncrypt.Length);
+            cs.FlushFinalBlock();
+            return Convert.ToBase64String(ms.ToArray());
+        }
     }
+}
 }
